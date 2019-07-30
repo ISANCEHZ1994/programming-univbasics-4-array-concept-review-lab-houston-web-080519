7083e8019f7c5c5562dd27923fa68d5048831a1d
Israel Sanchez <israel.pro1994@gmail.com>
2019-07-30 03:15:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,10 @@
 def find_element_index(array, value_to_find)
   
+  array.index(value_to_find)
   
-  
-  array.each do {|value_to_find| puts value_to_find}
-    value_to_find += 1
-  
-  
-  if value_to_find == []
-    then nil
-  end
  
+  
+  
 end
 
 def find_max_value(array)
